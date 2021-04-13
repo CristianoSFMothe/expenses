28d04393e6b94003d069c4f01e4811142e037c72
Cristiano Ferreira <cristianodevsystem@gmail.com>
2021-04-12 21:31:26 -0300

    Responsividade em textos

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final appBar = AppBar(
-      title: Text('Depesas Pessoais'),
+      title: Text(
+        'Depesas Pessoais',
+        style: TextStyle(fontSize: 20 * MediaQuery.of(context).textScaleFactor),
+      ),
       actions: <Widget>[
         IconButton(
           icon: Icon(Icons.add),
